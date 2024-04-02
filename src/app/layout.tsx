c0b31fbd483140db328fa74b6baa9039cb4d6382
mocaii <mocaimei@foxmail.com>
2024-04-02 19:00:43 +0800

    init 个人博客

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "灵感漫游记",
+  description: "资源收集，计划管理，学习笔记，个人博客",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
