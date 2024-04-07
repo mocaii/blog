7fe1a4f87745d61f14f9955cd9f647f98a54ba3d
mocaii <mocaimei@foxmail.com>
2024-04-07 20:10:33 +0800

    alter image

--- src/components/MarkdownRenderer/index.tsx
+++ src/components/MarkdownRenderer/index.tsx
@@ -5,11 +5,30 @@ import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
 import "github-markdown-css/github-markdown.css";
+import { useRouter } from "next/router";
+
+const ImageRenderer = ({ src, alt }: any) => {
+  const router = useRouter();
+  window.location.hostname;
+  const baseUrl =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000" // 开发环境下的基础路径
+      : window.location.origin; // 生产环境下的基础路径
+
+  return <img src={`${baseUrl}/${src}`} alt={alt} />;
+};
 
 function MarkdownRenderer({ markdown }: any) {
   return (
     <div className="markdown-body">
-      <Markdown rehypePlugins={[rehypeHighlight]}>{markdown}</Markdown>
+      <Markdown
+        rehypePlugins={[rehypeHighlight]}
+        components={{
+          img: ImageRenderer,
+        }}
+      >
+        {markdown}
+      </Markdown>
     </div>
   );
 }
